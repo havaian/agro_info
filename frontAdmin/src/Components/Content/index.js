9ac351872abe14f7c449430f30bf4174161745b5
Malik <m.usman.work@gmail.com>
2023-08-30 23:15:44 +0500

    Delete frontAdmin directory

--- frontAdmin/src/Components/Content/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export default function Content({ children, className }) {
-  return <div className={className + " content"}>{children}</div>;
-}
